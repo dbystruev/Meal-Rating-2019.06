a75f3d8b54eb927976d9f18b2963857ab39ff4cb
Denis Bystruev <dbystruev@me.com>
2019-06-04 22:34:05 +0300

    Simplified App Delegate

---
+++ MVC Example/App/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  MVC Example
+//
+//  Created by Denis Bystruev on 04/06/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
